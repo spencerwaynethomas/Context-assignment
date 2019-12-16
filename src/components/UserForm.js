cb4c6afa5a67b71605d521ef2ae7e0737eee6f36
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2019-12-15 23:02:16 -0700

    WIP

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -3,12 +3,42 @@ import { Form, } from "semantic-ui-react"
 import { UserConsumer, } from "../providers/UserProvider"
 
 class UserForm extends React.Component {
-  state = { firstName: "", lastName: "", }
+  state = { firstName: "", lastName: "", email: "", }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value, });
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
 
 
   render() {
+    const { firstName, lastName, email, } = this.state
     return (
-      <div> user form</div>
+      <Form onSubmit={this.handleSubmit} >
+        <Form.Input
+        label="First Name"
+        type="text"
+        name="firstName"
+        value={firstName}
+        onChange={this.handleChange}
+        />
+        <Form.Input
+        label="Last Name"
+        type="text"
+        name="lastName"
+        value={lastName}
+        onChange={this.handleChange}
+        />
+        <Form.Input
+        label="email"
+        type="text"
+        name="email"
+        value={email}
+        onChange={this.handleChange}
+        />
+      </Form>
     )
   }
 
